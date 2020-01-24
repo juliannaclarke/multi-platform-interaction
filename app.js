cd4f673bbf2509a0725cbd55fe1176e3c4870231
juliannaclarke <35417364+juliannaclarke@users.noreply.github.com>
2020-01-23 22:06:57 -0500

    created server

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+const LISTEN_PORT = 8080;
+
+app.use(express.static(__dirname + "/public")); //tells node where "home" is
+
+//routes
+app.get("/",function(req, res){
+    res.sendFile(__dirname +"public/index.html");
+});
+
+server.listen(LISTEN_PORT);
+console.log("Listening on port: " + LISTEN_PORT);
